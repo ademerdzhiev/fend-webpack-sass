225de1e127e8b79668715928e3fba06318447366
ademerdzhiev <a.demerdzhiev@gmail.com>
2020-11-22 15:34:57 +0200

    exercise api call challenge completed

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,3 +1,9 @@
+import {api_key} from './open_weather_map_api.js';
+const apiKey = api_key;
+const baseURL = 'https:/api.openweathermap.org/data/2.5/weather?q=';
+const city = 'Sofia';
+const country = 'Bulgaria';
+const location = 'Sofia,Bulgaria';
 function handleSubmit(event) {
     event.preventDefault()
 
@@ -6,10 +12,10 @@ function handleSubmit(event) {
     Client.checkForName(formText)
 
     console.log("::: Form Submitted :::")
-    fetch('http://localhost:8081/test')
+    fetch(baseURL+location+'&units=metric'+apiKey)
     .then(res => res.json())
     .then(function(res) {
-        document.getElementById('results').innerHTML = res.message
+        document.getElementById('results').innerHTML = 'Captain, ' + formText + ' your destination is ' + location + ' and the temperature there is ' +  res.main.temp + ' C.';
     })
 }
 
